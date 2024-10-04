b326cd6719150e180d296c3d87212410946eff85
amalshajimichaelk <amalshajimichaelk@gmail.com>
2024-10-04 16:05:39 +0530

    Add files via upload

---
+++ amal shaji michael.py
@@ -0,0 +1,35 @@
+'''print("Hello\nWorld")
+if 5>3:
+    print("Five is greater than Two")
+    print("Five is greater than Two")'''
+'''x=5
+x="thomas"
+print(x)'''
+'''x = 1j # x will be '3'
+y = int(3) # y will be 3
+z = float(3) # z will be 3.0
+print(type(x))
+print(type(y))
+print(type(z))
+a=3
+A= "amal"
+print(a)
+print(A)'''
+'''x, y, z = "orange", "banana", "cherry"
+print(x)
+print(y)
+print(z)
+x="Python "
+y="is "
+z="awesome"
+print(x+y+z)
+username=input("Enter Name")
+print("Username is: " + username)
+a="Hello"
+b="World"
+c=a+b
+print(c)'''
+a="Hello, World!"
+print(a[2:5])
+print(a[3])
+print(a[:5])
