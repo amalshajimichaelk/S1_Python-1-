265d32cfe5d7882177a562055ec0ade041ebb011
amalshajimichaelk <amalshajimichaelk@gmail.com>
2024-10-18 14:32:50 +0530

    Add files via upload

---
+++ basic_input.py
@@ -0,0 +1,14 @@
+'''
+Author: Amal Shaji Michael
+Date: 18-18-2024
+Description:program that asks the user to input
+ their name and age. After receiving the input,
+  display a message that includes the user's name and age.
+  Hello Amal! you are 18 years old
+'''
+
+
+
+name=input("Enter Your Name:")
+age=input("Enter your age:")
+print("Hello", name+"! you are",age,"years old" )
