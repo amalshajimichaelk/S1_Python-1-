f9220be74c6a8cd4556e35024f0ae63556a8de62
amalshajimichaelk <amalshajimichaelk@gmail.com>
2024-10-18 15:05:31 +0530

    Add files via upload

---
+++ basic_input_2.py
@@ -0,0 +1,18 @@
+'''
+Author: Amal Shaji Michael
+Date: 18-18-2024
+Description:  program that stores a string in a variable.
+Extract a specific part of the string (substring) and then concatenate it with another string.
+Finally, display the new string.
+World
+World Everyone!
+'''
+
+
+
+str="Hello World"
+stringr=str[6:12]
+print(stringr)
+string_s=" Everyone!"
+concatenate_string=stringr+string_s
+print(concatenate_string)
