9557c4df05315ccb59faa6e4849fc74f338132f7
Amal Shaji Michael <amalshajimichaelk@gmail.com>
2024-10-25 14:28:17 +0530

    Add files via upload

---
+++ lab_25-10-24.py
@@ -0,0 +1,6 @@
+number1=int(input("Enter the first number: "))
+number2=int(input("Enter the second number: "))
+if number1>number2:
+    print(number1," is greater than ",number2)
+else:
+    print(number2," is greater than ",number1)
