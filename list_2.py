7fc661fb496f61470c03ae7bfe85b69c7f3070bb
Amal Shaji Michael <183818109+amalshajimichaelk@users.noreply.github.com>
2024-11-22 14:09:43 +0530

    Add files via upload

---
+++ list_2.py
@@ -0,0 +1,16 @@
+list_1=[12,21,54,47,88,67]
+list_2=[20,13,34,5,66,91]
+combined_list=list_1+list_2
+even_list=[]
+odd_list=[]
+for i in combined_list:
+    if i%2==0:
+        even_list.append(i)
+    else:
+        odd_list.append(i)
+even_list.sort()
+odd_list.sort()
+combined_list=even_list+odd_list
+print(f"Even number list: {even_list}")
+print(f"Odd number list: {odd_list}")
+print(f"Combined list: {even_list}")
